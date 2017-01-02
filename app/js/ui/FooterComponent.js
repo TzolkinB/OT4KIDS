4c22962395cc01dbb1f5707fb0e5f2f5c1b01ec4
TzolkinB <krbell4@gmail.com>
2017-01-01 22:12:03 -0500

    Header and Footer added

--- app/js/ui/FooterComponent.js
+++ app/js/ui/FooterComponent.js
@@ -4,8 +4,18 @@ class FooterComponent extends React.Component {
 	render(){
 		return(
 			<div className="footer">
-				<div className="container">
-					<h1>Footer</h1>
+				<div className="container-fluid">
+				<hr/>
+					<div className="nav navbar-nav footer-buttons">
+						<button type="button" className="btn btn-default navbar-btn">Careers</button>
+					  <button type="button" className="btn btn-default navbar-btn">Employee Email</button>
+					</div>
+					<div className="nav navbar-nav navbar-right">
+			      <p className="footer-address">440 CENTRAL AVENUE | LEXINGTON, NC 27292</p>
+						<p className="footer-address">PHONE 336.236.6546 | FAX 336.236.9546</p>
+						<br/>
+						<p className="copyright">©2016 OT4KIDS INC. ALL RIGHTS RESERVED.</p>
+			    </div>
 				</div>
 			</div>
 		);
--- app/js/ui/NavComponent.js
+++ app/js/ui/NavComponent.js
@@ -6,53 +6,44 @@ class NavComponent extends React.Component {
 			<nav className="navbar navbar-default">
 			  <div className="container-fluid">
 			    <div className="navbar-header">
-			      <button type="button" className="navbar-toggle collapsed" 
-			      data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 			        <span className="sr-only">Toggle navigation</span>
 			        <span className="icon-bar"></span>
 			        <span className="icon-bar"></span>
 			        <span className="icon-bar"></span>
 			      </button>
-			      <a className="navbar-brand" href="#">Brand</a>
+			      <a className="navbar-brand" href="#">
+			      	<img src="/app/img/ot-logo.png" alt="OT4KIDS logo"/>
+		      	</a>
 			    </div>
 
 			    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			      <ul className="nav navbar-nav">
-			      	<li className="dropdown">
-			          <a href="#" className="dropdown-toggle" data-toggle="dropdown"
-			           role="button" aria-haspopup="true" aria-expanded="false">Therapy Services 
-			           <span className="caret"></span></a>
+			      	<li className="dropdown nav-links link-margin">
+			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Therapy Services<span className="caret"></span></a>
 			          <ul className="dropdown-menu">
-			            <li><a href="#">Services Offered</a></li>
-			            <li><a href="#">Clinic</a></li>
-			            <li><a href="#">Home Based Treatment</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">One more separated link</a></li>
+			            <li className="nav-links"><a href="#">Services Offered</a></li>
+			            <li className="nav-links"><a href="#">Clinic</a></li>
+			            <li className="nav-links"><a href="#">Home Based Treatment</a></li>
 			          </ul>
 			        </li>
-			        <li className="active"><a href="#">Testimonials <span className="sr-only">(current)</span></a></li>
-			        <li className="dropdown">
-			          <a href="#" className="dropdown-toggle" data-toggle="dropdown"
-			           role="button" aria-haspopup="true" aria-expanded="false">Staff 
-			           <span className="caret"></span></a>
+			        <li className="nav-links link-margin"><a href="#">Testimonials</a></li>
+			        <li className="dropdown nav-links">
+			          <a href="#" className="dropdown-toggle link-margin" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Staff <span className="caret"></span></a>
 			          <ul className="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">One more separated link</a></li>
+			            <li className="nav-links"><a href="#">Administrative Staff</a></li>
+			            <li className="nav-links"><a href="#">Occupational Therapists</a></li>
+			            <li className="nav-links"><a href="#">Physical Therapists</a></li>
+			            <li className="nav-links"><a href="#">Speech Therapists</a></li>
 			          </ul>
 			        </li>
-			        <li><a href="#">Link</a></li>
-
+			        <li className="nav-links link-margin"><a href="#">Contact</a></li>
+			        <li className="nav-links link-margin"><a href="#">Blog</a></li>
 			      </ul>
 			      
 			      <ul className="nav navbar-nav navbar-right">
-			        <li><a href="#">Link</a></li>
+			        <li><a href="#">Facebook</a></li>
+			        <li><a href="#">Instagram</a></li>
 			      </ul>
 			    </div>
 			  </div>
